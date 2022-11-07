5053351512ec6d9cc8cc2e6d835d46f68cd13f74
Jye <jyewang@qq.com>
2022-11-07 22:14:22 +0800

    updata

---
+++ bookmarks.js
@@ -0,0 +1,241 @@
+var bookmarks = {
+    常用:[
+        {
+            // imgsrc:'card.png',
+            title: '百度',
+            urlText: 'www.baidu.com',
+            url: 'https://www.baidu.com',
+            text: '百度搜索'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '搜狗',
+            urlText: 'www.sogou.com',
+            url: 'https://www.sogou.com',
+            text: '搜狗搜索，可以方便的搜索微信、知乎'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '必应',
+            urlText: 'cn.bing.com',
+            url: 'https://cn.bing.com',
+            text: '必应搜索'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '谷歌',
+            urlText: 'www.google.com',
+            url: 'https://www.google.com',
+            text: '谷歌搜索'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '哔哩哔哩',
+            urlText: 'www.bilibili.com',
+            url: 'https://www.bilibili.com',
+            text: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili'
+        },
+        {
+            // imgsrc:'card.png',
+            title: 'QQ 邮箱',
+            urlText: 'mail.qq.com',
+            url: 'https://mail.qq.com',
+            text: '良心的邮件服务，清爽无广告'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '幕布',
+            urlText: 'mubu.com',
+            url: 'https://mubu.com',
+            text: '思维导图只是副业，主业是记事本'
+        },
+        {
+            // imgsrc:'card.png',
+            title: 'Github',
+            urlText: 'github.com',
+            url: 'https://github.com',
+            text: '程序员必备技能 Ctrl  C、Download'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '百度翻译',
+            urlText: 'fanyi.baidu.com',
+            url: 'https://fanyi.baidu.com',
+            text: '还行，准确度还可以'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '新浪微博',
+            urlText: 'weibo.com',
+            url: 'https://weibo.com',
+            text: '实时热点还得微博搜'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '知乎',
+            urlText: 'www.zhihu.com',
+            url: 'https://www.zhihu.com',
+            text: '高...高质量人类社区'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '今日热榜',
+            urlText: 'tophub.today',
+            url: 'https://tophub.today',
+            text: '快速看全平台热榜'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '开发搜索',
+            urlText: 'kaifa.baidu.com',
+            url: 'https://kaifa.baidu.com',
+            text: '百度出品，还在beta，不过感觉不错'
+        }
+    ],
+    云盘:[
+        {
+            // imgsrc:'card.png',
+            title: '百度云盘',
+            urlText: 'pan.baidu.com',
+            url: 'https://pan.baidu.com',
+            text: '给个位置，但基本不再使用'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '阿里云盘',
+            urlText: 'www.aliyundrive.com',
+            url: 'https://www.aliyundrive.com',
+            text: '分享主力盘'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '和彩云',
+            urlText: 'yun.139.com',
+            url: 'https://yun.139.com',
+            text: '主力盘，毕竟国企，数据安全且有保证'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '小米云服务',
+            urlText: 'i.mi.com',
+            url: 'https://i.mi.com',
+            text: '手机资料同步'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '蓝奏云',
+            urlText: 'www.lanzou.com',
+            url: 'https://www.lanzou.com',
+            text: '小文件分享佳品'
+        },
+        {
+            // imgsrc:'card.png',
+            title: '夸克云盘',
+            urlText: 'pan.quark.cn',
+            url: 'https://pan.quark.cn',
+            text: '夸克的扩展服务'
+        }
+    ],
+    云服务商:[
+        {
+            // imgsrc:'card.png',
+            title: '腾讯云控制台',
+            urlText: 'console.cloud.tencent.com',
+            url: 'https://console.cloud.tencent.com',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: '阿里云控制台',
+            urlText: 'homenew.console.aliyun.com',
+            url: 'https://homenew.console.aliyun.com',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: 'Microsoft Azure',
+            urlText: 'portal.azure.com',
+            url: 'https://portal.azure.com',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: 'Oracle Cloud',
+            urlText: 'www.oracle.com',
+            url: 'https://www.oracle.com/cn/index.html',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: 'Oray 控制台',
+            urlText: 'console.oray.com',
+            url: 'https://console.oray.com',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: '蒲公英控制台',
+            urlText: 'console.sdwan.oray.com',
+            url: 'https://console.sdwan.oray.com',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: 'Heroku',
+            urlText: 'dashboard.heroku.com',
+            url: 'https://dashboard.heroku.com',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: 'Cloudflare',
+            urlText: 'www.cloudflare.com',
+            url: 'https://www.cloudflare.com/zh-cn/',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: 'Freenom',
+            urlText: 'www.freenom.com',
+            url: 'https://www.freenom.com/zh/index.html',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: 'DDNSTO',
+            urlText: 'www.ddnsto.com',
+            url: 'https://www.ddnsto.com',
+            text: ''
+        }
+    ],
+    以太坊:[
+        {
+            // imgsrc:'card.png',
+            title: '欧易',
+            urlText: 'www.okx.com',
+            url: 'https://www.okx.com',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: '欧易池',
+            urlText: 'www.okx.com/pool/pow',
+            url: 'https://www.okx.com/pool/pow',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: 'Ezil',
+            urlText: 'ezil.me',
+            url: 'https://ezil.me',
+            text: ''
+        },
+        {
+            // imgsrc:'card.png',
+            title: 'Etherchain',
+            urlText: 'etherchain.org',
+            url: 'https://etherchain.org',
+            text: '查询 GAS'
+        }
+    ]
+};
